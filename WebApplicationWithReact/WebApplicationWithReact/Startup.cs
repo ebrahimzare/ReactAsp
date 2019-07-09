beaac58d822d0a415920905c4ea0d997bbdfc7fa
MAHOOR\Ebi <zare.math@gmail.com>
2019-07-09 08:57:48 -0400

    Project donne

---
+++ WebApplicationWithReact/WebApplicationWithReact/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationWithReact.Startup))]
+namespace WebApplicationWithReact
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
